cde3bb26a30f7082def4256a3fd814d2b688f70d
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2023-11-03 12:04:08 +0100

    basic Js added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("hello");
+var totalDrumKeys = document.querySelectorAll(".drum").length;
+console.log(totalDrumKeys);
+
+for (var i = 0; i < totalDrumKeys; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleclick);
+}
+
+function handleclick() {
+  alert("I am clicked");
+}
