0e86312dfe679f0df88420a125fefb9c5508c47b
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2023-11-19 14:44:10 +0100

    audio track added to buttons

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var totalDrumKeys = document.querySelectorAll(".drum").length;
 console.log(totalDrumKeys);
 
-for (var i = 0; i < totalDrumKeys; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleclick);
-}
+var audioSource = {
+  j: "./sounds/crash.mp3",
+  k: "./sounds/kick-bass.mp3",
+  l: "./sounds/snare.mp3",
+  w: "./sounds/tom-1.mp3",
+  a: "./sounds/tom-2.mp3",
+  s: "./sounds/tom-3.mp3",
+  d: "./sounds/tom-4.mp3",
+};
 
-function handleclick() {
-  alert("I am clicked");
+for (var i = 0; i < totalDrumKeys; i++) {
+  console.log("inside for loop i =" + i);
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("click", function (event) {
+      var clickedKey = event.target.innerText;
+      console.log(clickedKey);
+      var audio = new Audio(audioSource[clickedKey]);
+      audio.play();
+    });
 }
